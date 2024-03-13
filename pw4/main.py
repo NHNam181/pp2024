5f4d29591330d72dc3b7f28402573db6869a42e6
Nguyen Hai Nam <nhnam180104@gmail.com>
2024-03-14 02:21:59 +0700

    restored

--- pw4/main.py
+++ pw4/main.py
@@ -3,6 +3,9 @@ from domains.student import Student
 from domains.course import Course
 from input import input_int
 import numpy as np
+import os.path
+
+save_path = 'D:/pp2024/pw5/'
 
 def main():
     student_info = []
@@ -35,6 +38,18 @@ def main():
     for course in course_info:
         print(f"\nCourse Name: {course.name}\nCourse ID: {course.ID}")
 
+    with open("students.txt", "w") as students_file:
+        for student in student_info:
+            students_file.write(f"{student.ID}.{student.Name} - {student.DoB}\n")
 
+    with open("courses.txt", "w") as courses_file:
+        for course in course_info:
+            courses_file.write(f"{course.ID}.{course.name}\n")
+
+    with open("marks.txt", "w") as marks_file:
+        for student in student_info:
+            for course in course_info:
+                marks_file.write(f"{student.ID}.{student.Name}:{course.name} - {student.Marks[course.name]}\n")
+    
 if __name__ == "__main__":
     main()
