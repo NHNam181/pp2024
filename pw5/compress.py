10f50683ea11f0be27fc0293612e8205e7b38b07
Nguyen Hai Nam <nhnam180104@gmail.com>
2024-03-14 02:27:06 +0700

    Add file compressor

---
+++ pw5/compress.py
@@ -0,0 +1,15 @@
+import zipfile
+
+def compression(filenames, compressed_file, compression_method):
+    with zipfile.ZipFile(compressed_file, "w") as zipf:
+        for filename in filenames:
+            zipf.write(filename, compress_type=compression_method)
+
+if __name__ == "__main__":
+    filenames = ["students.txt", "courses.txt", "marks.txt"]
+
+    compressed_file = "students.dat"
+
+    compression_method = zipfile.ZIP_DEFLATED
+
+    compression(filenames, compressed_file, compression_method)
